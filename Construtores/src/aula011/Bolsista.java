efa2aca36d4f00bfa3e213474560cfa313289249
Rafael <vasconcelos_90@hotmail.co.jp>
2018-10-25 23:08:40 -0300

    Exclusão do projeto Anagram Game

---
+++ Construtores/src/aula011/Bolsista.java
@@ -0,0 +1,25 @@
+package aula011;
+
+public class Bolsista extends Aluno {
+    
+    private float bolsa;
+    
+    public void renovarBolsa(){
+        
+    }
+    
+    
+    @Override
+    public void pagarMensalidade(){
+        
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
+    
+}
