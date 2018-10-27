fbfa4290e0b9fae513e766420f0e0bdb01182735
Rafael <vasconcelos_90@hotmail.co.jp>
2018-10-27 17:45:52 -0300

    Renomeado para Encapsulamento

---
+++ Encapsulamento/src/aula06/Aula06.java
@@ -0,0 +1,11 @@
+package aula06;
+
+public class Aula06  {
+
+    public static void main(String[] args) {
+        Teste t = new Teste();
+        t.setA(32);
+        t.setB(54);
+        System.out.println(t.getB()+ " " + t.getA());
+    }
+}
---
+++ Encapsulamento/src/aula06/Teste.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula06;
+
+/**
+ *
+ * @author vasco
+ */
+public class Teste {
+    
+    private int a;
+    private int b;
+
+    public Teste() {
+    }
+
+    
+    
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+}
